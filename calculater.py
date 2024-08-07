e5aea2ccb7719b2cf1416741cac07f60003b8596
Mahadeo Kr <mahadeo.t@bigdoorsolutions.co.in>
2024-08-07 11:09:01 +0530

    first assignment

---
+++ calculater.py
@@ -0,0 +1,34 @@
+# addition...
+def sum(a, b):
+    c = a + b
+    return c 
+
+# multiplication....
+def mul(m, n):
+    res = m * n
+    return res
+
+# substraction....
+def sub(a, b):
+    c = a - b
+    return c
+
+# division....
+def div(a, b):
+    c = a / b
+    return c
+    
+
+
+diff = sub(10, 5)
+print(f"sub of two no is: {diff}")
+
+product = mul(3, 3)
+print(f"multiplication of m and n is : {product}" )
+
+add = sum(40, 50)
+print(f"sum of the number: {add}") 
+
+division = div(50, 5)
+print(f"div of two no is: {division}")
+
